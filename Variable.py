2e9c9bea95d3b7643e86a4c393e1bc1170d5a3ba
BrayamE <145371884+BrayamE@users.noreply.github.com>
2023-09-24 23:24:31 -0500

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+#BRAYAM EDWIN QUISPE APAZA
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = DecisionTreeClassifier()
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Precisión del modelo: {accuracy:.2f}")
